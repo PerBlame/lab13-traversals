04de04b6aafc57140b7797137bb2613baa5bca6a
Leo Truong <leoc.truong@gmail.com>
2022-05-06 13:32:52 -0700

    updated traversal order

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -125,7 +125,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	private void preOrderRecurse(BSTNode<T> node) {
 		if (node == null)
 			return;
-		System.out.println(node.data);
+		System.out.print(node.data + " ");
 		
 		preOrderRecurse(node.leftChild);
 		preOrderRecurse(node.rightChild);
